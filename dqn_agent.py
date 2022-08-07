24f8867a62499bb42fef78bbb1f4cc5bad4af06a
RyanLiu <1036433215@qq.com>
2022-08-07 23:00:43 +0800

    Update dqn_agent.py

--- dqn_agent.py
+++ dqn_agent.py
@@ -15,7 +15,7 @@ parser.add_argument('--env', type=str, default="parking_env-v0", help='name of t
 parser.add_argument('--render', type=bool, default=False, help='render the environment')
 parser.add_argument('--seed', type=int, default=0, metavar='N', help='random seed (default: 0)')
 parser.add_argument('--total_timesteps', type=int, default=int(5e6), help='total timesteps to run')
-parser.add_argument('--save_freq', type=int, default=int(1e5), help='checkpoint save frequency')
+parser.add_argument('--save_freq', type=int, default=int(5e5), help='checkpoint save frequency')
 parser.add_argument('--log_path', type=str, default='./log', help='logging path')
 parser.add_argument('--ckpt_path', type=str, default='', help='checkpoint path')
 parser.add_argument('--mode', type=str, default='3', choices=['1', '2', '3', '4', '5'], help='mode')
