b7d752cdbd17cc0194f7e3501822fd8181b855cf
RyanLiu <1036433215@qq.com>
2022-08-08 15:36:58 +0800

    Update CustomEnv.py

--- parking_env/parking_env/env/CustomEnv.py
+++ parking_env/parking_env/env/CustomEnv.py
@@ -66,7 +66,9 @@ class CustomEnv(gym.GoalEnv):
 
         # self.reward_weights = np.array([1, 0.3, 0, 0, 0.02, 0.02])
         if self.mode == '4':
-            self.reward_weights = np.array([0.5, 0.8, 0, 0, 0.2, 0.2])
+            self.reward_weights = np.array([0.4, 0.9, 0, 0, 0.15, 0.15])
+        elif self.mode == '5':
+            self.reward_weights = np.array([0.7, 0.6, 0, 0, 0.2, 0.2])
         else:
             self.reward_weights = np.array([1, 0.3, 0, 0, 0.1, 0.1])
         self.target_orientation = None
@@ -111,7 +113,7 @@ class CustomEnv(gym.GoalEnv):
         # p.addUserDebugLine([3.5, 3.5, 0.02], [3.5, -3.5, 0.02], [0.75, 0.75, 0.75], 5)
         # p.addUserDebugLine([3.5, 3.5, 0.02], [-3.5, 3.5, 0.02], [0.75, 0.75, 0.75], 5)
 
-        # mode = 1, 2 (右上)
+        # # mode = 1, 2 (右上)
         # self.left_wall = p.loadURDF(os.path.join(self.base_path, "assets/up/side_boundary.urdf"), basePosition=[1.3, 2.1, 0.03], useFixedBase=10)
         # self.right_wall = p.loadURDF(os.path.join(self.base_path, "assets/up/side_boundary.urdf"), basePosition=[2.5, 2.1, 0.03], useFixedBase=10)
         # self.front_wall = p.loadURDF(os.path.join(self.base_path, "assets/up/front_boundary_ru.urdf"), basePosition=[1.9, 2.8, 0.03], useFixedBase=10)
@@ -134,13 +136,13 @@ class CustomEnv(gym.GoalEnv):
         # p.addUserDebugLine([-3.4, 1.5, 0.02], [-3.4, 2.7, 0.02], [0.98, 0.98, 0.98], 2.5)
 
         # mode = 4 (左下)
-        self.left_wall = p.loadURDF(os.path.join(self.base_path, "assets/down/side_boundary_ld.urdf"), basePosition=[-0.8, -2.0, 0.03], useFixedBase=10)
-        self.right_wall = p.loadURDF(os.path.join(self.base_path, "assets/down/side_boundary_ld.urdf"), basePosition=[-2.8, -2.0, 0.03], useFixedBase=10)
-        self.front_wall = p.loadURDF(os.path.join(self.base_path, "assets/down/front_boundary_ld.urdf"), basePosition=[-1.8, -2.5, 0.03], useFixedBase=10)
+        self.left_wall = p.loadURDF(os.path.join(self.base_path, "assets/down/side_boundary_ld.urdf"), basePosition=[-0.8, -2.1, 0.03], useFixedBase=10)
+        self.right_wall = p.loadURDF(os.path.join(self.base_path, "assets/down/side_boundary_ld.urdf"), basePosition=[-2.8, -2.1, 0.03], useFixedBase=10)
+        self.front_wall = p.loadURDF(os.path.join(self.base_path, "assets/down/front_boundary_ld.urdf"), basePosition=[-1.8, -2.7, 0.03], useFixedBase=10)
         # p.addUserDebugLine([-0.9, -1.6, 0.02], [-2.7, -1.6, 0.02], [0.98, 0.98, 0.98], 2.5)
-        # p.addUserDebugLine([-0.9, -1.6, 0.02], [-0.9, -2.4, 0.02], [0.98, 0.98, 0.98], 2.5)
-        # p.addUserDebugLine([-2.7, -2.4, 0.02], [-2.7, -1.6, 0.02], [0.98, 0.98, 0.98], 2.5)
-        # p.addUserDebugLine([-2.7, -2.4, 0.02], [-0.9, -2.4, 0.02], [0.98, 0.98, 0.98], 2.5)
+        # p.addUserDebugLine([-0.9, -2.6, 0.02], [-2.7, -2.6, 0.02], [0.98, 0.98, 0.98], 2.5)
+        # p.addUserDebugLine([-0.9, -1.6, 0.02], [-0.9, -2.6, 0.02], [0.98, 0.98, 0.98], 2.5)
+        # p.addUserDebugLine([-2.7, -1.6, 0.02], [-2.7, -2.6, 0.02], [0.98, 0.98, 0.98], 2.5)
 
         # mode = 5 (右下)
         # self.left_wall = p.loadURDF(os.path.join(self.base_path, "assets/down/side_boundary_rd.urdf"), basePosition=[1.7, -2.3, 0.03], useFixedBase=10)
@@ -172,7 +174,7 @@ class CustomEnv(gym.GoalEnv):
             self.start_orientation = [0, 0, np.pi * 4 / 2]
             self.target_orientation = np.pi * 3 / 2
         elif self.mode == '4':
-            self.goal = np.array([-3.6 / 2, -4.0 / 2])
+            self.goal = np.array([-3.6 / 2, -4.2 / 2])
             self.start_orientation = [0, 0, np.pi * 0 / 2]
             self.target_orientation = np.pi * 0 / 2
         elif self.mode == '5':
