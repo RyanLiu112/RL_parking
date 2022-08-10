fe5e066f728aa34d6375924eceeb1754787c246d
RyanLiu <1036433215@qq.com>
2022-08-10 16:31:57 +0800

    Update setup.py

--- parking_env/setup.py
+++ parking_env/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='parking_env',
     version='0.0.1',
-    install_requires=['gym', 'numpy', 'pybullet', 'stable-baselines3']
+    install_requires=['gym', 'numpy', 'pybullet', 'stable-baselines3', 'torch', 'moviepy']
 )
