79590b37617c82bbd15a8938b0529f42d9ca12dc
RyanLiu <1036433215@qq.com>
2022-08-10 17:13:13 +0800

    Update dqn_agent.py

--- dqn_agent.py
+++ dqn_agent.py
@@ -16,10 +16,10 @@ parser.add_argument('--env', type=str, default="parking_env-v0", help='name of t
 parser.add_argument('--render', type=bool, default=False, help='render the environment')
 parser.add_argument('--seed', type=int, default=0, help='random seed (default: 0)')
 parser.add_argument('--total_timesteps', type=int, default=int(2e6), help='total timesteps to run')
-parser.add_argument('--save_freq', type=int, default=int(1000), help='checkpoint save frequency')
+parser.add_argument('--save_freq', type=int, default=int(5e5), help='checkpoint save frequency')
 parser.add_argument('--log_path', type=str, default='./log', help='logging path')
 parser.add_argument('--ckpt_path', type=str, default='', help='checkpoint path')
-parser.add_argument('--mode', type=str, default='2', choices=['1', '2', '3', '4', '5'], help='mode')
+parser.add_argument('--mode', type=str, default='1', choices=['1', '2', '3', '4', '5', '6'], help='mode')
 
 args = parser.parse_args()
 
@@ -49,8 +49,6 @@ del model
 env = gym.make(args.env, render=True, mode=args.mode)
 obs = env.reset()
 model = DQN.load(args.ckpt_path, env=env)
-# mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
-# print(f'mean reward: {mean_reward}, std reward: {std_reward}')
 
 episode_return = 0
 for i in range(1000):
